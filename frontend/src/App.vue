a2f574d6114fd09bb9077b3f29471b1b08a3c147
Alex <plast.r.alex@gmail.com>
2020-01-08 22:00:02 -0500

    remove duplicate iframe

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="#app" class="center-content-wrapper">
+  <div
+    id="#app"
+    class="center-content-wrapper"
+  >
     <div v-if="isMorning==null">
       <!-- Do nothing until response -->
     </div>
-    <div v-else-if="isMorning" id="is-morning">
-      <iframe 
-      width="755"
-      height="425"
-      src="https://www.youtube.com/embed/BIikfdNIHQE?autoplay=1&controls=0&loop=1&modestbranding=1&disablekb=1%html5=True"
-      frameborder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-      id="video"
-      ></iframe>
-      <iframe 
-      width="400"
-      height="300"
-      src="https://www.youtube.com/embed/bsnyIrj8CM4?autoplay=1&controls=0&loop=1&modestbranding=1&disablekb=1%html5=True"
-      frameborder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-      id="videomobile"
+    <div
+      v-else-if="isMorning"
+      id="is-morning"
+    >
+      <iframe
+        width="755"
+        height="425"
+        src="https://www.youtube.com/embed/BIikfdNIHQE?autoplay=1&controls=0&loop=1&modestbranding=1&disablekb=1%html5=True"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+        id="video"
       ></iframe>
       <div class="content">
         It's {{this.currentTimeText}} in Vietnam.
       </div>
     </div>
-    <div v-else id="not-morning">
+    <div
+      v-else
+      id="not-morning"
+    >
       <div class="NO">NO</div>
       <div class="But-if-you-come-back">
         But if you come back in about {{this.timeToNextMorningText}}, it just might be.
@@ -36,61 +36,59 @@
 </template>
 
 <script>
-import Axios from 'axios'
+import Axios from "axios";
 
 export default {
-  name: 'app',
-  components: {
-  },
-    data(){
+  name: "app",
+  components: {},
+  data() {
     return {
       isMorning: null,
       timeToNextMorningText: null,
-      currentTimeText: null,
-    }
+      currentTimeText: null
+    };
   },
-  created(){
+  created() {
     //check whether it is morning, and the time to next morning, and current time text
     //but you only get the current time if it is morning, and time to next morning if it is not morning
-    Axios.get("https://istitmorninginvietnamapi.azurewebsites.net/getifitismorninginvietnam")
-    .then((response) => {
-      this.isMorning = response.data.isMorning
-      this.timeToNextMorningText = response.data.timeToNextSunriseText
-      this.currentTimeText = response.data.currentTimeText
-      
-    })
-
+    Axios.get(
+      "https://istitmorninginvietnamapi.azurewebsites.net/getifitismorninginvietnam"
+    ).then(response => {
+      this.isMorning = response.data.isMorning;
+      this.timeToNextMorningText = response.data.timeToNextSunriseText;
+      this.currentTimeText = response.data.currentTimeText;
+    });
   },
-  ready(){
+  ready() {
     //check the time every 5 minutes
     window.setInterval(() => {
-      Axios.get("https://istitmorninginvietnamapi.azurewebsites.net/getifitismorninginvietnam&showinfo=0")
-      .then((response) => {
-        this.isMorning = response.data.isMorning
-        this.timeToNextMorningText = response.data.timeToNextSunriseText
-        this.currentTimeText = response.data.currentTimeText
-      })
-    }, 5000)
+      Axios.get(
+        "https://istitmorninginvietnamapi.azurewebsites.net/getifitismorninginvietnam&showinfo=0"
+      ).then(response => {
+        this.isMorning = response.data.isMorning;
+        this.timeToNextMorningText = response.data.timeToNextSunriseText;
+        this.currentTimeText = response.data.currentTimeText;
+      });
+    }, 5000);
   },
-  methods:{
+  methods: {
     play: function() {
-            document.getElementById('video').click()
-
-      document.getElementById('video').play()
+      document.getElementById("video").click();
+      document.getElementById("video").play();
     }
   },
-  computed:{
+  computed: {
     morning() {
-      return this.isMorning
+      return this.isMorning;
     }
   }
-}
+};
 </script>
 
 <style>
-#app {  
+#app {
 }
-.center-content-wrapper{
+.center-content-wrapper {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   font-family: acumin-pro-wide, sans-serif;
@@ -102,45 +100,45 @@ export default {
   align-items: center;
   height: 100vh;
 }
-#video{
+#video {
   position: fixed;
   bottom: 0;
-  left:0;
+  left: 0;
   min-width: 100%;
   min-height: 100%;
 }
 .content {
   position: fixed;
   bottom: 0;
-  left:0;
-  background-image:linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1));
+  left: 0;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
   color: #f1f1f1;
   min-width: 100%;
   padding: 20px;
   opacity: 1;
-	animation-name: fadeInOpacity;
-	animation-iteration-count: 1;
-	animation-timing-function: ease-in;
-	animation-duration: 0.5s;
+  animation-name: fadeInOpacity;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in;
+  animation-duration: 0.5s;
 }
 
-#not-morning{
+#not-morning {
   opacity: 1;
-	animation-name: fadeInOpacity;
-	animation-iteration-count: 1;
-	animation-timing-f3unction: ease-in;
-	animation-duration: 0.5s;
+  animation-name: fadeInOpacity;
+  animation-iteration-count: 1;
+  animation-timing-f3unction: ease-in;
+  animation-duration: 0.5s;
 }
-.NO{
+.NO {
   font-size: 144pt;
 }
-.But-if-you-come-back{
-  font-weight:200;
+.But-if-you-come-back {
+  font-weight: 200;
   font-style: normal;
 }
-.centerwrapper{
-  width:100%;
-  height:100%;
+.centerwrapper {
+  width: 100%;
+  height: 100%;
   margin: 0 auto;
 }
 
@@ -159,37 +157,36 @@ a {
   color: #42b983;
 }
 @keyframes fadeInOpacity {
-	0% {
-		opacity: 0;
-	}
-	100% {
-		opacity: 1;
-	}
-}
-@media (pointer:coarse){
-  #video{
-    display:none;
+  0% {
+    opacity: 0;
   }
-  #videomobile{
-      position: fixed;
-  bottom: 0;
-  left:0;
-  min-width: 100%;
-  min-height: 100%;
+  100% {
+    opacity: 1;
   }
 }
-
-@media (hover:none){
-    #video{
-    display:none;
+@media (pointer: coarse) {
+  #video {
+    display: none;
   }
-  #videomobile{
-      /* position: fixed; */
-  /* bottom: 0; */
-  /* left:0; */
-  /* min-width: 100%; */
-  /* min-height: 100%; */
+  #videomobile {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    min-width: 100%;
+    min-height: 100%;
   }
 }
 
+@media (hover: none) {
+  #video {
+    display: none;
+  }
+  #videomobile {
+    /* position: fixed; */
+    /* bottom: 0; */
+    /* left:0; */
+    /* min-width: 100%; */
+    /* min-height: 100%; */
+  }
+}
 </style>
